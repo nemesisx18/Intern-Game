d9859085fc7dd608b877ebff236a266f4d817f1c
ihzamaldy_27 <ihzam.aldy27@gmail.com>
2021-09-27 13:43:20 +0700

    Update 2 angka dibelakang koma

--- Assets/RandomNumber.cs
+++ Assets/RandomNumber.cs
@@ -12,16 +12,52 @@ public class RandomNumber : MonoBehaviour
     public TextMeshProUGUI txt_hasil;
     public TMP_InputField txt_input;
 
-    int angka1, angka2, angka3 ,aritmatika;
+    double angka1, angka2, angka3 ,aritmatika;
 
-    float hasil, inputJawaban;
+    public double hasil, inputJawaban;
+    
+
+    private void Start()
+    {
+        angka1 = Random.Range(0, 10);
+        angka2 = Random.Range(0, 10);
+        aritmatika = Random.Range(0, 4);
+
+        if (aritmatika == 0)
+        {
+            txt_aritmatika.text = " + ";
+            hasil = angka1 + angka2;
+        }
+        else if (aritmatika == 1)
+        {
+            txt_aritmatika.text = " - ";
+            hasil = angka1 - angka2;
+        }
+        else if (aritmatika == 2)
+        {
+            txt_aritmatika.text = " * ";
+            hasil = angka1 * angka2;
+        }
+        else if (aritmatika == 3)
+        {
+            txt_aritmatika.text = " / ";
+            double a, b;
+            a = angka1;
+            b = angka2;
+            hasil = a / b;
+        }
+
+        txt_angka1.text = angka1.ToString();
+        txt_angka2.text = angka2.ToString();
+        txt_hasil.text = hasil.ToString("0.##");
+    }
 
     private void Update()
     {
         txt_input.text = inputJawaban.ToString();
     }
 
-    public void inputJawabanKunci(float jawaban)
+    public void inputJawabanKunci(double jawaban)
     {
         inputJawaban = jawaban;
         Debug.Log(inputJawaban);
@@ -58,7 +94,7 @@ public class RandomNumber : MonoBehaviour
 
             txt_angka1.text = angka1.ToString();
             txt_angka2.text = angka2.ToString();
-            txt_hasil.text = hasil.ToString();
+            txt_hasil.text = hasil.ToString("0.##");
         }
     }
 
@@ -87,10 +123,12 @@ public class RandomNumber : MonoBehaviour
         {
             txt_aritmatika.text = " / ";
             hasil = angka1 / angka2;
+            
         }
-
+        
+        //float a = Mathf.Round(hasil);
         txt_angka1.text = angka1.ToString();
         txt_angka2.text = angka2.ToString();
-        txt_hasil.text = hasil.ToString("F2");
-    }
+        txt_hasil.text = hasil.ToString("0.##");
+    } 
 }
