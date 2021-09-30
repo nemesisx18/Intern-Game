5f616404675404a7d965278bc531f5f10030a50c
Abdul003 <73937383+nemesisx18@users.noreply.github.com>
2021-09-30 19:55:53 +0700

    nambah trap

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] trap;
-    
+    public GameObject[] trap2;
+
     float elapsedTime = 0f; // Counts up to repeatTime
     float repeatTime = 4f; // Time taken to repeat in seconds
 
     private int currentIndex = 0;
+    private int currentIndex2 = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -33,13 +35,18 @@ public class GameManager : MonoBehaviour
     public void RandomSpike()
     {
         int newIndex = Random.Range(0, trap.Length);
+        int newIndex2 = Random.Range(0, trap2.Length);
         
         // Deactivate old gameobject
         trap[currentIndex].SetActive(false);
-        
+        trap2[currentIndex2].SetActive(false);
+
         // Activate new gameobject
         currentIndex = newIndex;
+        currentIndex2 = newIndex2;
         trap[currentIndex].SetActive(true);
+        trap2[currentIndex2].SetActive(true);
     }
-
 }
+
+
